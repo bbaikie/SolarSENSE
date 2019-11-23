e36b4ae4d9948bb7e4f8675e24f8e4124dc7a6ab
david <david.plotzke@gmail.com>
2019-11-23 07:26:19 +0000

    Changes: 1) Modified the settings.py file to access postgresql. Notes: To get the database to work requires external configuration. Specifically you need to install both PostgreSQL and psycopg2.        After you need to create a database that corrisponds with the settings specified in settings.py. Finally, to do this you need        to use the command sudo -u postgres psql. From there you can add the database and user specified in the settings.py.w

--- hub/src/Website/Website/settings.py
+++ hub/src/Website/Website/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'solarsense',
+	'USER':'dmplotzke',
+	'PASSWORD': '}Zk67c)TEj8~:k]:',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
