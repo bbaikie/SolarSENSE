e19829b9c67554fac2d568b936c1512881483e6f
david <david.plotzke@gmail.com>
2020-01-22 04:25:48 +0000

    Created the model for the sensor and started adding the logic to list the test data in the test webpage.

--- hub/src/Website/SolarSENSE/models.py
+++ hub/src/Website/SolarSENSE/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class SensorData(models.Model):
+	temperature = models.DecimalField(max_digits=5, decimal_places=2)
+	water = models.DecimalField(max_digits=5, decimal_places=2)
+	phosphate = models.DecimalField(max_digits=5, decimal_places=2)
+	sunshine = models.DecimalField(max_digits=5, decimal_places=2)
--- hub/src/Website/SolarSENSE/views.py
+++ hub/src/Website/SolarSENSE/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import SensorData
 
 def test(request):
     return HttpResponse('This is a test page')
