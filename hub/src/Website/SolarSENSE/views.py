fc7556289d13c8493d5ef36968ff588714abe2cd
david <david.plotzke@gmail.com>
2020-02-03 22:44:01 +0000

    Modified views.py to test that static and media images work correctly.

--- hub/src/Website/SolarSENSE/views.py
+++ hub/src/Website/SolarSENSE/views.py
@@ -7,9 +7,8 @@ def test(request):
     return HttpResponse('This is a test page')
 
 def templateTest(request):
-    video = Video.objects.get(name = 'test')
-    context = {"video": video,}
-    template = loader.get_template('templateTest.html')
-    return HttpResponse(template.render(context, request))
+    video = Video.objects.get(name = "test")
+    context = {"video":video}
+    return render(request, "templateTest.html", context)
 
 
