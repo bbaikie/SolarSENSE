f1e533cb18873d48c00ecdd475ed0f5122204d4b
bbgun7 <jpm@hiya.ca>
2020-02-11 21:23:37 -0700

    Task 217 Made skeleton functions for sensor

---
+++ sensor/src/corefunctionality.cpp
@@ -0,0 +1,29 @@
+void activateLowPowerMode(int sleepSeconds){
+    //TODO fill in
+}
+
+void sampleAndStoreTemperature(){
+    //TODO fill in
+}
+void sampleAndStoreMoisture(){
+    //TODO fill in
+}
+void sampleAndStorePhosphate(){
+    //TODO fill in
+}
+void sampleAndStoreSunlight(){
+    //TODO fill in
+}
+
+void transmitStoredTemperature(){
+    //TODO fill in
+}
+void transmitStoredMoisture(){
+    //TODO fill in
+}
+void transmitStoredPhosphate(){
+    //TODO fill in
+}
+void transmitStoredSunlight(){
+    //TODO fill in
+}
---
+++ sensor/src/corefunctionality.h
@@ -0,0 +1,11 @@
+void activateLowPowerMode(int sleepSeconds);
+
+void sampleAndStoreTemperature();
+void sampleAndStoreMoisture();
+void sampleAndStorePhosphate();
+void sampleAndStoreSunlight();
+
+void transmitStoredTemperature();
+void transmitStoredMoisture();
+void transmitStoredPhosphate();
+void transmitStoredSunlight();
