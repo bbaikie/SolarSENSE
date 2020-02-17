19bbec1390f38c354dfff742e39a6776d8c06c5a
David <davidplotzke@gmail.com>
2020-02-17 16:51:56 -0700

    Test script improvements

--- hub/src/Website/scripts/generateTags.py
+++ hub/src/Website/scripts/generateTags.py
@@ -1,29 +1,55 @@
 from SolarSENSE.models import Video
 from SolarSENSE.models import CustomTag
-#import random
+import random
 
 def run():
 
+    vidNum = 5
+    tempTagNum = 5
+    sunTagNum = 5
+    waterTagNum = 5
+    phosTagNum = 5
+
+    # Videos
+    for x in range(vidNum):
+        Video.objects.create(name="Test" + str(x), content="Test" + str(x) + ".jpg")
+
     # Temperature
-#    for x in range(5):
-#        min = random.uniform(0, 100)
-#        max = random.uniform(0, 100)
-#        Video.objects.create(min=min ,max=max , name=str(min) + " <-> " + str(max), type="temperature")
+    for x in range(tempTagNum):
+        min = random.uniform(0, 100)
+        max = random.uniform(0, 100)
+        num = random.randrange(0, vidNum)
+
+        t = CustomTag.objects.create(min=min ,max=max , name=str(min) + " <-> " + str(max), type="temperature")
+        v = Video.objects.get(name="Test" + str(num))
+        v.tags.add(t)
 
     # Sunlight
-   # for x in range(5):
-   #     min = random.uniform(0, 100)
-   #     max = random.uniform(0, 100)
-   #     Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="sunlight")
+    for x in range(sunTagNum):
+        min = random.uniform(0, 100)
+        max = random.uniform(0, 100)
+        num = random.randrange(0, vidNum)
+
+        CustomTag.objects.create(min=min ,max=max , name=str(min) + " <-> " + str(max), type="sunlight")
+        v = Video.objects.get(name="Test" + str(num))
+        v.tags.add(t)
 
     # Water
-   # for x in range(5):
-   #     min = random.uniform(0, 100)
-   #     max = random.uniform(0, 100)
-   #     Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="water")
+    for x in range(waterTagNum):
+        min = random.uniform(0, 100)
+        max = random.uniform(0, 100)
+        num = random.randrange(0, vidNum)
+
+        t = CustomTag.objects.create(min=min ,max=max , name=str(min) + " <-> " + str(max), type="water")
+        v = Video.objects.get(name="Test" + str(num))
+        v.tags.add(t)
 
     # Phosphate
-   # for x in range(5):
-   #     min = random.uniform(0, 100)
-   #     max = random.uniform(0, 100)
-   #     Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="phosphate")
+    for x in range(phosTagNum):
+        min = random.uniform(0, 100)
+        max = random.uniform(0, 100)
+        num = random.randrange(0, vidNum)
+
+        t = CustomTag.objects.create(min=min ,max=max , name=str(min) + " <-> " + str(max), type="phosphate")
+        v = Video.objects.get(name="Test" + str(num))
+        v.tags.add(t)
