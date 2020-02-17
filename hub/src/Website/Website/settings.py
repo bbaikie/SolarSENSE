fce107c74c8b7622aae02742a7ef2a064b612701
david <david.plotzke@gmail.com>
2020-02-17 23:35:08 +0000

    Added django-extensions so can use runscript to run python scripts within the Django context.

--- hub/src/Website/Website/settings.py
+++ hub/src/Website/Website/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'SolarSENSE',
     'taggit',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- hub/src/Website/scripts/generateTags.py
+++ hub/src/Website/scripts/generateTags.py
@@ -1,28 +1,29 @@
 from SolarSENSE.models import Video
 from SolarSENSE.models import CustomTag
-import random
+#import random
 
 def run():
+
     # Temperature
-    for x in range(5):
-        min = random.uniform(0, 100)
-        max = random.uniform(0, 100)
-        Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="temperature")
+#    for x in range(5):
+#        min = random.uniform(0, 100)
+#        max = random.uniform(0, 100)
+#        Video.objects.create(min=min ,max=max , name=str(min) + " <-> " + str(max), type="temperature")
 
     # Sunlight
-    for x in range(5):
-        min = random.uniform(0, 100)
-        max = random.uniform(0, 100)
-        Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="sunlight")
+   # for x in range(5):
+   #     min = random.uniform(0, 100)
+   #     max = random.uniform(0, 100)
+   #     Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="sunlight")
 
     # Water
-    for x in range(5):
-        min = random.uniform(0, 100)
-        max = random.uniform(0, 100)
-        Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="water")
+   # for x in range(5):
+   #     min = random.uniform(0, 100)
+   #     max = random.uniform(0, 100)
+   #     Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="water")
 
     # Phosphate
-    for x in range(5):
-        min = random.uniform(0, 100)
-        max = random.uniform(0, 100)
-        Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="phosphate")
+   # for x in range(5):
+   #     min = random.uniform(0, 100)
+   #     max = random.uniform(0, 100)
+   #     Video.objects.create(min=min ,max=max , name=str(min) + " -> " + str(max), type="phosphate")
