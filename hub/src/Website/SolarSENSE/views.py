3774c96744ed8236cd5d69b944673b54ce51e52a
David <davidplotzke@gmail.com>
2020-02-19 16:52:29 -0700

    Added in queries to get videos with appropriate tags and started writing the results page.y

--- hub/src/Website/SolarSENSE/views.py
+++ hub/src/Website/SolarSENSE/views.py
@@ -18,6 +18,16 @@ def tagTest(request):
     return render(request, "tagTest.html", context)
 
 def showTags(request):
+    temp = request.GET["temperature"]
+    water = request.GET["water"]
+    phosphate = request.GET["phosphate"]
+    sunlight = request.GET["sunlight"]
+
+    videos = Video.objects.filter(tags__type__exact="temperature", tags__min__lte=temp, tags__max__gte=temp) |
+    Video.objects.filter(tags__type__exact="water", tags__min__lte=water, tags__max__gte=water) |
+    Video.objects.filter(tags_type_exact="phosphate", tags__min_lte=phosphate, tags__max_gte=phosphate) |
+    Video.objects.filter(tags_type_exact="sunlight", tags_min_lte=sunlight, tags_max_gte=sunlight)
     context = {
+        videos : "videos"
     }
-    return HttpResponse("This will show the tags")
+    return render(request, "tagResult.html", context)
