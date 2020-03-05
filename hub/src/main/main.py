3b7f874848894198d4de33f802e2b7b55fc04ffd
Tanner <tannerzuran@outlook.com>
2020-03-04 23:41:28 -0700

    Updated temperature method to include start of model as well as adding the JSON object for temperature into that model.

--- hub/src/main/main.py
+++ hub/src/main/main.py
@@ -1,15 +1,25 @@
 from jsonrpcserver import method, serve
+from django.db import models
 # This is used to start an instance of a server
 
 # Might need to include a method or someway to store the data
 # into a list or database to use later
 def main():
 
+class Data(models.Model):
+	temperature_readings = models.DoubleField(default=0.0)
+	
+
 # List of methods to grab all 4 points of data
 @jsonrpc_method('myapp.setData')
 def storeTemp(request, temperature):
-    #database.add(temperature)
+    #for x in (temperature.JSONArray.length)
+    	#data = new 		#Data(temperature_readings=(temperature.JSONArray(x)))
+    #data.save()
     return temperature
+
+	#How to loop through and save as different data objects? or do 
+	#we want to do that?
 
 @jsonrpc_method('myapp.setData')
 def storeMoisture(request, moisture):
