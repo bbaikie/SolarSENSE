5363b7d5db863501319221cb858c59996720f421
bbgun7 <jpm@hiya.ca>
2020-03-19 19:06:59 -0700

    Initial code to test connecting sensor to hub and calling jsonrpc functions

--- sensor/src/main.ino
+++ sensor/src/main.ino
@@ -1,8 +1,12 @@
-//#include <jsonrpccpp/client/connectors/httpclient.h>
+#include <jsonrpccpp/client/connectors/httpclient.h>
+#include "gen/stubclient.h"
+
 #include "corefunctionality.h"
 //I don't know why, but any esp32 libraries included in corefunctionality.h need to be included here
 #include <WiFi.h>
 #include <Preferences.h>
+
+using namespace jsonrpc;
 
 const char* ssid "NameOfNetwork"
 const char* password "AardvarkBadgerHedgehog"
@@ -15,24 +19,40 @@ void setup() {
 
 void loop() {
     //sending four list to perspective sampel and store functions
+    /*
     sampleAndStoreTemperature();
     sampleAndStoreMoisture();
     sampleAndStorePhosphate();
     sampleAndStoreSunlight();
+    */
 
     //TODO put in correct wifi info
     connectToWifi(ssid, password);
 
     if (WiFi.status() == WL_CONNECTED) {
         //connected to wifi
-        //TODO get jsonrpc stuff set up
-        //HttpClient httpclient(WiFi.localIP());
+        HttpClient httpclient(WiFi.localIP());
+        StubClient jsonClient(httpclient, JSONRPC_CLIENT_V2);
+
+        try {
+            testRetArr = jsonClient.sendTemperatureData(testArr);
+            if(testArr == testRetArr) {
+                Serial.println("Success!");
+            } else {
+                Serial.println("Failure :(");
+            }
+        } catch(JsonRpcException &e) {
+            Serial.print("Error: ");
+            Serial.println(e.what());
+        }
 
         //TODO these functions may need no have jsonrpc stuff passed to them so they can transmit properly
+        /*
         transmitStoredTemperature();
         transmitStoredMoisture();
         transmitStoredPhosphate();
         transmitStoredSunlight();
+        */
     }
 
     //Go into low power mode for 6 minutes
