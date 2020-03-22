4d4419528cac37957ccda09e66175f89eb864e44
david <david.plotzke@gmail.com>
2020-03-22 00:27:14 +0000

    Extended the admin list html file.

--- hub/src/Website/SolarSENSE/admin.py
+++ hub/src/Website/SolarSENSE/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Video
 
-admin.site.register(Video)
+class BatchUploadAdmin(admin.ModelAdmin):
+    change_list_template = "admin/video_upload_list.html"
+
+admin.site.register(Video, BatchUploadAdmin)
+
