f568f9ddd10b67dd65489444b843788310c64fed
David <davidplotzke@gmail.com>
2020-03-23 11:38:30 -0700

    Added a form to the cusom list view and experimented with passing custom context values into the viewy

--- hub/src/Website/SolarSENSE/admin.py
+++ hub/src/Website/SolarSENSE/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Video
+from .forms import VideoUploadForm
 
 class BatchUploadAdmin(admin.ModelAdmin):
     change_list_template = "admin/video_upload_list.html"
