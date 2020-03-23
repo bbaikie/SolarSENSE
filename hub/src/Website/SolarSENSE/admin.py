95280ab506bd69541b3c8924a00641e43654a79b
David <davidplotzke@gmail.com>
2020-03-23 12:03:21 -0700

    More experimenting with forms and the hooks provided by admin

--- hub/src/Website/SolarSENSE/admin.py
+++ hub/src/Website/SolarSENSE/admin.py
@@ -4,6 +4,10 @@ from .forms import VideoUploadForm
 
 class BatchUploadAdmin(admin.ModelAdmin):
     change_list_template = "admin/video_upload_list.html"
+    
+    # def changelist_view(self, request, extra_context=None):
+    #     extra_context = extra_context or {}
+    #     return super(BatchUploadAdmin, self).changelist_view(request, extra_context=extra_context)
 
 admin.site.register(Video, BatchUploadAdmin)
 
--- hub/src/Website/SolarSENSE/urls.py
+++ hub/src/Website/SolarSENSE/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.test, name='test'),
     path('templateTest/', views.templateTest, name='templateTest'),
     path('tagTest/', views.tagTest, name="tagTest"),
-    path('tagTest/showTags/', views.showTags, name="showTags")
+    path('tagTest/showTags/', views.showTags, name="showTags"),
 ]
