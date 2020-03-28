a2432bf7f966888fc6ef1bd388c2e15bf13556fa
David <davidplotzke@gmail.com>
2020-03-27 19:27:03 -0700

    Stopped using the custom tag functionality and added an admin action for adding tags to all selected items. The action will redirect to a page where tags can be selected. Currently it will just trigger a notification that tags have been added.

--- hub/src/Website/SolarSENSE/admin.py
+++ hub/src/Website/SolarSENSE/admin.py
@@ -3,6 +3,12 @@ from .models import Video
 
 class BatchUploadAdmin(admin.ModelAdmin):
     change_list_template = "admin/video_upload_list.html"
+    actions = ["add_tags"]
+
+    def add_tags(self, request, queryset):
+        self.message_user(request, "Successfully added tag")
+    add_tags.short_description = "Add Tags"
 
 admin.site.register(Video, BatchUploadAdmin)
+
 
