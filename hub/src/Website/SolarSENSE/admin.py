abe786cecbf25ba553be3d740c45441f8a928e27
David <davidplotzke@gmail.com>
2020-03-27 20:06:16 -0700

    Modified the url to pass the primary keys that can be used to access the list of tags to modify.

--- hub/src/Website/SolarSENSE/admin.py
+++ hub/src/Website/SolarSENSE/admin.py
@@ -12,7 +12,9 @@ class BatchUploadAdmin(admin.ModelAdmin):
         selected = queryset.values_list("pk", flat=True)
         ct = ContentType.objects.get_for_model(queryset.model)
         # self.message_user(request, "Successfully added tag")
-        return HttpResponseRedirect("/changeTags")
+        return HttpResponseRedirect("/changeTags/?ct=%s&ids=%s" % (
+            ct.pk, ','.join(str(pk) for pk in selected),
+        ))
     add_tags.short_description = "Add Tags"
 
 admin.site.register(Video, BatchUploadAdmin)
