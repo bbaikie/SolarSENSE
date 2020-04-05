6ddda2a68d060dbb2b38230bf5b41fe723b8ab74
david <david.plotzke@gmail.com>
2020-04-05 04:10:33 +0100

    Started on the setTemp method.

--- hub/src/Website/SolarSENSE/__init__.py
+++ hub/src/Website/SolarSENSE/__init__.py
@@ -3,3 +3,8 @@ from rpc4django import rpcmethod
 @rpcmethod(name='ping', signature=[])
 def ping():
     return "pong"
+
+@rpcmethod(name='setTemp', signature=['array'])
+def setTemp(request, list):
+    from .models import Video
+    return list
