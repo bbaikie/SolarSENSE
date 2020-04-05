41b4181c1a9f0b1aef8d43b2a0c2ef9ed0b8b0bf
David <davidplotzke@gmail.com>
2020-04-04 20:54:12 -0700

    Added the functions for moisture, phosphate, and sunlight.

--- hub/src/Website/SolarSENSE/__init__.py
+++ hub/src/Website/SolarSENSE/__init__.py
@@ -27,3 +27,75 @@ def setTemp(request, inputArr):
             del request.session['temperature']
         else:
             raise Exception("Number of values in response mismatched")
+
+@rpcmethod(name='setMoisture', signature=['array'])
+def setMoisture(request, inputArr):
+    from .models import SensorCollections
+    request.session['moisture'] = inputArr
+
+    if 'phosphate' in request.session and 'sunlight' in request.session and 'temperature' in request.session:
+        phosphate = request.session['phosphate']
+        sunlight = request.session['sunlight']
+        moisture = request.session['moisture']
+        temperature = request.session['temperature']
+
+        size = len(phosphate)
+
+        if size == len(sunlight) and size == len(moisture) and size == len(temperature):
+            for x in range(size):
+                SensorCollections.objects.create(moisture=moisture[x], temperature=temperature[x], sunlight=sunlight[x], phosphate=phosphate[x])
+            
+            del request.session['phosphate']
+            del request.session['sunlight']
+            del request.session['moisture']
+            del request.session['temperature']
+        else:
+            raise Exception("Number of values in response mismatched")
+
+@rpcmethod(name='setSun', signature=['array'])
+def setSun(request, inputArr):
+    from .models import SensorCollections
+    request.session['sunlight'] = inputArr
+
+    if 'phosphate' in request.session and 'temperature' in request.session and 'moisture' in request.session:
+        phosphate = request.session['phosphate']
+        sunlight = request.session['sunlight']
+        moisture = request.session['moisture']
+        temperature = request.session['temperature']
+
+        size = len(phosphate)
+
+        if size == len(sunlight) and size == len(moisture) and size == len(temperature):
+            for x in range(size):
+                SensorCollections.objects.create(moisture=moisture[x], temperature=temperature[x], sunlight=sunlight[x], phosphate=phosphate[x])
+            
+            del request.session['phosphate']
+            del request.session['sunlight']
+            del request.session['moisture']
+            del request.session['temperature']
+        else:
+            raise Exception("Number of values in response mismatched")
+
+@rpcmethod(name='setPhos', signature=['array'])
+def setPhos(request, inputArr):
+    from .models import SensorCollections
+    request.session['phosphate'] = inputArr
+
+    if 'temperature' in request.session and 'sunlight' in request.session and 'moisture' in request.session:
+        phosphate = request.session['phosphate']
+        sunlight = request.session['sunlight']
+        moisture = request.session['moisture']
+        temperature = request.session['temperature']
+
+        size = len(phosphate)
+
+        if size == len(sunlight) and size == len(moisture) and size == len(temperature):
+            for x in range(size):
+                SensorCollections.objects.create(moisture=moisture[x], temperature=temperature[x], sunlight=sunlight[x], phosphate=phosphate[x])
+            
+            del request.session['phosphate']
+            del request.session['sunlight']
+            del request.session['moisture']
+            del request.session['temperature']
+        else:
+            raise Exception("Number of values in response mismatched")
