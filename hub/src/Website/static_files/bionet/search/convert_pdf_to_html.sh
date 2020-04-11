10b57622eb94a77f356c8dbfcce4984865f4e306
david <david.plotzke@gmail.com>
2020-04-12 00:58:47 +0100

    Deleted a copy of the bionet directory.

--- hub/src/Website/static_files/bionet/search/convert_pdf_to_html.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-while IFS= read -r line; do
-	echo "Processing $line"
-	pdf2htmlEX $line --dest-dir $(dirname "$line") --process-nontext 0 --embed-external-font 0
-done <searchable_pdf_files.txt
