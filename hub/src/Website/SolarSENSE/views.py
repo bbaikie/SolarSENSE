b75ebb725037f50ee0c240c0f25b38b804570606
David <davidplotzke@gmail.com>
2020-04-11 17:16:47 -0700

    Removed testing code and added some comments.

--- hub/src/Website/SolarSENSE/views.py
+++ hub/src/Website/SolarSENSE/views.py
@@ -21,64 +21,17 @@ from django.db.models import Case, CharField, Value, When, Count
 from django.contrib.contenttypes.models import ContentType
 from django.views.generic import ListView
 
+# For testing purposes
 def test(request):
     return HttpResponse('This is a test page')
 
-def templateTest(request):
-    mediaImage = Video.objects.get(name = "testMedia")
-    context = {
-        "media":mediaImage
-    }
-    return render(request, "templateTest.html", context)
-
+# Shows list of videos
 class VideosView(ListView):
     paginate_by = 7
     model = Video
     template_name = "list.html"
 
-def tagTest(request):
-    context = {}
-    return render(request, "tagTest.html", context)
-
-def showTags(request):
-    temp = 0
-    water = 0
-    sunlight = 0
-    phosphate = 0
-    try:
-        temp = request.POST["temperature"]
-        water = request.POST["water"]
-        sunlight = request.POST["sunlight"]
-        phosphate = request.POST["phosphate"]
-    except (KeyError):
-        HttpResponse("The value was invalid")
-
-    allVideos = Video.objects.all()
-
-    waterSet = allVideos.filter(tags__type="water", tags__min__lte=water, tags__max__gte=water)
-    waterSet = allVideos.difference(allVideos.filter(tags__type="water")).union(waterSet)
-
-    tempSet = allVideos.filter(tags__type="temperature", tags__min__lte=temp, tags__max__gte=temp)
-    tempSet = allVideos.difference(allVideos.filter(tags__type="temperature")).union(tempSet)
-
-    sunSet = allVideos.filter(tags__type="sunlight", tags__min__lte=sunlight, tags__max__gte=sunlight)
-    sunSet = allVideos.difference(allVideos.filter(tags__type="sunlight")).union(sunSet)
-
-    phosSet = allVideos.filter(tags__type="phosphate", tags__min__lte=phosphate, tags__max__gte=phosphate)
-    phosSet = allVideos.difference(allVideos.filter(tags__type="phosphate")).union(phosSet)
-
-    videos = waterSet.intersection(tempSet, phosSet, sunSet)
-    
-    names = []
-    for x in videos:
-        names.append(x.tags.names())
-
-    context = {
-        "videos":videos,
-        "nameSet":tuple(names)
-    }
-    return render(request, "tagResult.html", context)
-
+# Writes uploaded videos to database and handles upload to file system
 def uploadVideos(request):
     if request.method == "POST":
         files = request.FILES.getlist('videos')
@@ -86,6 +39,7 @@ def uploadVideos(request):
             Video.objects.create(name=f.name, content=f)
     return HttpResponseRedirect(reverse("admin:SolarSENSE_video_changelist"))
 
+# Generates the page to add tags to all videos selected in admin.
 def changeTags(request):
     if request.method == "GET":
         request.session["ct"] = request.GET["ct"]
@@ -102,16 +56,10 @@ def changeTags(request):
                 video.tags.add(tag)
     return HttpResponseRedirect(reverse("admin:SolarSENSE_video_changelist"))
 
+# Renders the statistics page
 def stats(request):
-  
     sensorQuery = SensorCollections.objects.all() 
     context = {
         "query":sensorQuery 
     }
     return render(request,'stats.html',context)  
-
-# def importTagSettings(request):
-#     return HttpResponseRedirect(reverse("admin:SolarSENSE_video_changelist"))
-
-# def exportTagSettings(request):
-#     return HttpResponseRedirect(reverse("admin:SolarSENSE_video_changelist"))
